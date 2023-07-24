febca866d0d77552dbefd8e4e7efd3a340d1a67f
Michael Champlin <mchamplin@sbcglobal.net>
2023-07-24 13:24:45 -0700

    Implement Delete Client Button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,25 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import Clients from "./components/Clients";
 
+const cache = new InMemoryCache({  // Must change code below in "const client" replace InMemoryCache with cache
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
